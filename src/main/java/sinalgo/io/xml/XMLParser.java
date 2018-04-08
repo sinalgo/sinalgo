c82b3bbbab081025dd93bf2b0a6d090b511275c2
Andre Brait <andrebrait@gmail.com>
2018-04-08 15:20:34 -0300

    Upgrading JDOM from 1.1.3 to JDOM2 2.0.6

--- src/main/java/sinalgo/io/xml/XMLParser.java
+++ src/main/java/sinalgo/io/xml/XMLParser.java
@@ -41,11 +41,11 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
-import org.jdom.Attribute;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
+import org.jdom2.Attribute;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 import sinalgo.configuration.Configuration;
 import sinalgo.runtime.Global;
